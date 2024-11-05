4cdfdda16191a29612d5e6758e9a3dadc648d5bb
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2024-11-05 17:29:44 +0100

    ADD versione con axios

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const emailList = document.getElementById("email-list");
-const beginButton = document.getElementById("comincia")
+const beginButton = document.getElementById("comincia");
 const reGenButton = document.getElementById("rigenera");
-
+// versione con il fetch
 function emailGenerator() {
   let emailHtml = "";
   for (let i = 0; i < 10; i++) {
@@ -16,13 +16,26 @@ function emailGenerator() {
 }
 
 beginButton.addEventListener("click", () => {
-    beginButton.classList.add("d-none")
-    reGenButton.classList.remove("d-none")
-    emailGenerator()
-})
+  beginButton.classList.add("d-none");
+  reGenButton.classList.remove("d-none");
+  emailGenerator();
+});
 reGenButton.addEventListener("click", () => {
-    emailList.innerHTML = "";
-    emailGenerator()
-})
+  emailList.innerHTML = "";
+  emailGenerator();
+});
 
 
+// versione con axios
+// function emailGenerator() {
+//   let emailHtml = "";
+//   for (let i = 0; i < 10; i++) {
+//     axios
+//       .get("https://flynn.boolean.careers/exercises/api/random/mail")
+//       .then((response) => {
+//         emailHtml += `<li class="list-group-item text-bg-dark">${response.data.response}</li>`;
+//         emailList.innerHTML = emailHtml;
+//       })
+//       .catch((error) => console.error("Errore nel fetch", error));
+//   }
+// }
